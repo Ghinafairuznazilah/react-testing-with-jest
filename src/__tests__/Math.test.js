feaf9eccef3150fd9f22dd02fe6681444e950266
Ghinafairuznazilah <ghina.fairuz.nazilah@gmail.com>
2021-11-14 22:11:24 +0700

    add answer math test

--- src/__tests__/Math.test.js
+++ src/__tests__/Math.test.js
@@ -3,31 +3,43 @@ import { Add, Substract, Multiplication } from '../Math'
 test('Return sum of two arguments', () => {
   // Arrange
 
+  const a = 20;
+  const b = 10;
+  const excpected = 30;
   // Act
-
+ 
+  const calculate = Add(a,b)
 
   // Assert
-
+  expect(calculate).toBe(excpected)
+  
 })
 
 test('Return substraction of a - b', () => {
-  // Arrange
+   // Arrange
 
-
-  // Act
-
-
-  // Assert
+   const a = 20;
+   const b = 10;
+   const excpected = 10;
+   // Act
+  
+   const calculate = Substract(a,b)
+ 
+   // Assert
+   expect(calculate).toBe(excpected)
 
 })
 
 test('Return multiplication of two arguments', () => {
-  // Arrange
+   // Arrange
 
-
-  // Act
-
-
-  // Assert
-
+   const a = 20;
+   const b = 10;
+   const excpected = 200;
+   // Act
+  
+   const calculate = Multiplication(a,b)
+ 
+   // Assert
+   expect(calculate).toBe(excpected)
 })
